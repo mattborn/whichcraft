5f344d66da9543d4fe76ee93f2f6fd972e2c1dd0
Matt Born <me+code@mattborn.com>
2015-04-26 21:32:54 -0500

    Add Status

--- app.jsx
+++ app.jsx
@@ -1157,17 +1157,108 @@ var Upgrade = React.createClass({
 });
 
 var Status = React.createClass({
+  getInitialState: function() {
+    return {
+      online: false,
+      unique_brews: null,
+      unique_breweries: null,
+      total_users: null
+    };
+  },
+  componentWillMount: function() {
+    var self = this;
+    firebase.once('value', function(snapshot) {
+      // TODO: replace total_brews with recursive count of each users’ brews
+      self.setState({
+        online: true,
+        unique_brews: snapshot.child('brews').numChildren(),
+        unique_breweries: snapshot.child('breweries').numChildren(),
+        total_brews: snapshot.child('brews').numChildren(),
+        total_users: snapshot.child('users').numChildren()
+      });
+    });
+  },
   render: function () {
     var style = {
-      maxWidth: 375,
-      margin: '0 auto'
+      status: {
+        maxWidth: 375,
+        margin: '0 auto',
+        padding: '30px 0 0'
+      },
+      title: {
+        fontSize: Type.large,
+        margin: '0 0 20px'
+      },
+      server: {
+        border: '1px solid '+ Colors.dark,
+        display: 'inline-block',
+        fontSize: Type.medium,
+        lineHeight: '38px',
+        padding: '0 20px'
+      },
+      quadrant: {
+        width: '50%',
+        borderColor: Colors.dark,
+        borderStyle: 'solid',
+        float: 'left',
+        padding: 25
+      },
+      first: { borderWidth: '0 1px 1px 0' },
+      second: { borderWidth: '0 0 1px 0' },
+      third: { borderWidth: '0 1px 0 0' },
+      fourth: { borderWidth: 0 },
+      metric: {
+        font: '36px proxima-nova, sans-serif',
+        margin: '10px 0 0'
+      },
+      label: {
+        letterSpacing: '.1em',
+        textTransform: 'uppercase'
+      },
+      from: {
+        color: Colors.light,
+        fontSize: Type.small,
+        margin: '15px 0 0'
+      },
+      heading: { fontSize: Type.medium }
     };
+    _.extend(style.status, scrollStyles);
     return (
-      <Center>
-        <div style={style}>
-          Status
+      <div style={style.status}>
+        <h1 style={style.title}>Status</h1>
+        <span style={style.server}>Server is {this.state.online ? 'ONLINE' : <span style={style.offline}>OFFLINE</span>}</span>
+        <div>
+          <div style={_.extend(style.first, style.quadrant)}>
+            <p style={style.metric}>{this.state.unique_brews}</p>
+            <p style={style.label}>Brews</p>
+            <p style={style.from}>from</p>
+            <p style={style.metric}>{this.state.unique_breweries}</p>
+            <p style={style.label}>Breweries</p>
+          </div>
+          <div style={_.extend(style.second, style.quadrant)}>
+            <p style={style.metric}>{this.state.total_brews}</p>
+            <p style={style.label}>Total Brews</p>
+            <p style={style.from}>from</p>
+            <p style={style.metric}>{this.state.total_users}</p>
+            <p style={style.label}>Users</p>
+          </div>
+          <div style={_.extend(style.third, style.quadrant)}>
+            <p style={style.heading}>Active Users</p>
+            <p style={style.metric}>--</p>
+            <p style={style.label}>Past Day</p>
+            <p style={style.metric}>--</p>
+            <p style={style.label}>Past Week</p>
+          </div>
+          <div style={_.extend(style.fourth, style.quadrant)}>
+            <p style={style.heading}>Brews Added</p>
+            <p style={style.metric}>--</p>
+            <p style={style.label}>Past Day</p>
+            <p style={style.metric}>{this.state.total_users}</p>
+            <p style={style.label}>Past Week</p>
+          </div>
+          <div style={{clear:'both'}} />
         </div>
-      </Center>
+      </div>
     );
   }
 });
